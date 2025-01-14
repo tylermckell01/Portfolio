2000e50246cc5190a943419ff49fa0c89af732f2
Tyler McKell <mckelltyler@gmail.com>
2025-01-14 16:50:18 -0700

    gpt setup started, doesnt function correctly yet, need to make more secure

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,31 @@
 import { useState } from "react";
+import OpenAI from "openai";
 
 export default function Hero() {
   const [chatInput, setChatInput] = useState("");
+  const [gptResponse, setGptResponse] = useState("");
 
-  const handlesubmit = (e) => {
+  const openai = new OpenAI({
+    apiKey:
+      "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",
+    dangerouslyAllowBrowser: true,
+  });
+
+  const handlesubmit = async (e) => {
     e.preventDefault();
 
-    console.log("chatInput:", chatInput);
+    const completion = await openai.chat.completions.create({
+      model: "gpt-4o-mini",
+      messages: [
+        { role: "system", content: "You are a helpful assistant." },
+        {
+          role: "user",
+          content: chatInput,
+        },
+      ],
+    });
+
+    setGptResponse(completion.choices[0].message.content);
   };
 
   return (
@@ -22,7 +41,7 @@ export default function Hero() {
           <div className="buttons">
             <button type="submit">submit</button>
           </div>
-          <div className="gpt-response">this will be the gpt response</div>
+          <div className="gpt-response">{gptResponse}</div>
         </form>
       </div>
       <div className="right">
