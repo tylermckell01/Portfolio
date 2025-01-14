6f29067a0ec42118eec6f06644a865d697ea56a2
Tyler McKell <mckelltyler@gmail.com>
2025-01-13 18:36:35 -0700

    initial header setup

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <div className="entire">
+      <div className="left">Tyler McKell</div>
+      <div className="right">
+        <div>link</div>
+        <div>link</div>
+        <div>link</div>
+        <div>link</div>
+      </div>
+    </div>
+  );
+}
