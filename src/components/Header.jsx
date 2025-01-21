cebd3098025e1e9dd17575d1b159e0e2f7efe3a9
Tyler McKell <mckelltyler@gmail.com>
2025-01-21 13:08:23 -0700

    adds xp files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ export default function Header() {
     <div className="header">
       <div className="left">Tyler McKell</div>
       <div className="right">
-        <a href="./App.js">JS</a>
-        <a href="./App.js">REACT</a>
-        <a href="./App.js">PYTHON</a>
+        <a href="./MyJs.js">JS</a>
+        <a href="./MyReact.js">REACT</a>
+        <a href="./MyPython.js">PYTHON</a>
         <a href="./App.js">link</a>
         <a href="./App.js">link</a>
       </div>
